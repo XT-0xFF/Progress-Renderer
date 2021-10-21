cd1a83d4587e7f290560c37f19b9f54d46de1092
Chris F <CF-public@gmx.net>
2021-10-21 20:28:15 +0200

    Added Harmony patches to disable HeatMap and LightMap overlays when rendering the map for a screenshot

---
+++ Source/Harmony_Patches/Harmony_HeatMap.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using Verse;
+
+namespace ProgressRenderer
+{
+	public static class Harmony_HeatMap
+	{
+		public static void TryPatch(Harmony harmony)
+		{
+			try
+			{
+				var type = Type.GetType("HeatMap.Main, HeatMap");
+				if (type != null)
+				{
+					harmony.Patch(
+						AccessTools.Method(type, "UpdateHeatMap"),
+						prefix: new HarmonyMethod(typeof(Harmony_HeatMap), nameof(Harmony_HeatMap.Prefix)));
+				}
+			}
+			catch (Exception)
+			{ }
+		}
+
+		public static bool Prefix()
+		{
+			return Find.CurrentMap?.GetComponent<MapComponent_RenderManager>()?.Rendering != true;
+		}
+	}
+}
