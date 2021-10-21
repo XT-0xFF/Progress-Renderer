2cf8a5fdae98164343ede7643d54f20612b07acf
Chris F <CF-public@gmx.net>
2021-10-21 20:47:20 +0200

    Added Harmony patch which should disable all overlays, including those generated by the HeatMap and LightMap mods
    Skipping MapInterface.MapInterfaceUpdate disables:
    	targeter.TargeterUpdate
    	SelectionDrawer.DrawSelectionOverlays
    	EnvironmentStatsDrawer.DrawRoomOverlays
    	designatorManager.DesignatorManagerUpdate
    	selector.gotoController.Draw
    	roofGrid.RoofGridUpdate
    	fertilityGrid.FertilityGridUpdate
    	terrainGrid.TerrainGridUpdate
    	exitMapGrid.ExitMapGridUpdate
    	deepResourceGrid.DeepResourceGridUpdate
    as well as several debug overlays.

---
+++ Source/Harmony_Patches/Harmony_MapInterface.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace ProgressRenderer
+{
+	[HarmonyPatch(typeof(MapInterface))]
+	[HarmonyPatch(nameof(MapInterface.MapInterfaceUpdate))]
+	public static class Harmony_MapInterface
+	{
+		public static bool Prefix()
+		{
+			return Find.CurrentMap?.GetComponent<MapComponent_RenderManager>()?.Rendering != true;
+		}
+	}
+}
